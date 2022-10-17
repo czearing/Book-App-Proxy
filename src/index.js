05ec996fe869677b5627108cb1d1e6a0c56f35ce
czearing <czearing@outlook.com>
2022-10-17 15:01:56 -0700

    Adding files

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import router from "./routes/index.js";
+import * as dotenv from "dotenv";
+// import rateLimit from "express-rate-limit";
+
+dotenv.config();
+
+const PORT = process.env.port || 5000;
+
+const app = express();
+
+// Rate Limiting
+// const limiter = rateLimit({
+//   windowMs: 10 * 60 * 1000,
+//   max: 5,
+// });
+// app.use(limiter);
+app.set("trust proxy", 1);
+
+// Enable cors
+app.use(cors());
+
+// Routes
+app.use("/api", router);
+
+// Start on port
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
